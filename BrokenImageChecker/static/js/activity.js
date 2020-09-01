77e39f124d356e71cb080b04c09fb14d1759a8a4
Mert <bilgic.mert44@hotmail.com>
2020-09-01 16:53:02 +0300

    Added demo progressbar

--- BrokenImageChecker/static/js/activity.js
+++ BrokenImageChecker/static/js/activity.js
@@ -3,6 +3,22 @@ $(document).ready(function(){
         $.ajax({url: "/linkcheck", success: function(result){
         console.log(result);
         }});
+        $("#progress").show();
+        demo();
     });
 });
+var progressbar = new Nanobar({target: document.getElementById('progress')});
+var start = 10;
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+async function demo() {
+    for(var i=1;i<=10;i++){
+        status= start*i
+        progressbar.go(status);
+        await sleep(1000);
+    }
+    $("#progress").hide();
+    progressbar.go(0);
+}
   
